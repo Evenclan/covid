e85dea4f1b8099ae3e5c339b84e55e64e71c2738
Kamil Bogucki <boguckikamil@hotmail.com>
2020-04-03 12:00:36 +0200

    change country array

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-const apiJoke = 'https://api.covid19api.com/summary';
+const covid = 'https://api.covid19api.com/summary';
 
 async function getData() {
-    const response = await fetch(apiJoke);
+    const response = await fetch(covid);
     const data = await response.json();
 
-    const NewConfirmedPoland = data.Countries[164].NewConfirmed;
-    const TotalConfirmedPoland = data.Countries[164].TotalConfirmed;
-    const NewDeathsPoland = data.Countries[164].NewDeaths;
-    const TotalDeathsPoland = data.Countries[164].TotalDeaths;
-    const NewRecoveredPoland = data.Countries[164].NewRecovered;
-    const TotalRecoveredPoland = data.Countries[164].TotalRecovered;
+    console.log(data.Countries);
 
-    const NewConfirmedPortugal = data.Countries[165].NewConfirmed;
-    const TotalConfirmedPortugal = data.Countries[165].TotalConfirmed;
-    const NewDeathsPortugal = data.Countries[165].NewDeaths;
-    const TotalDeathsPortugal = data.Countries[165].TotalDeaths;
-    const NewRecoveredPortugal = data.Countries[165].NewRecovered;
-    const TotalRecoveredPortugal = data.Countries[165].TotalRecovered;
+    const NewConfirmedPoland = data.Countries[165].NewConfirmed;
+    const TotalConfirmedPoland = data.Countries[165].TotalConfirmed;
+    const NewDeathsPoland = data.Countries[165].NewDeaths;
+    const TotalDeathsPoland = data.Countries[165].TotalDeaths;
+    const NewRecoveredPoland = data.Countries[165].NewRecovered;
+    const TotalRecoveredPoland = data.Countries[165].TotalRecovered;
 
-    const NewConfirmedSweden = data.Countries[199].NewConfirmed;
-    const TotalConfirmedSweden = data.Countries[199].TotalConfirmed;
-    const NewDeathsSweden = data.Countries[199].NewDeaths;
-    const TotalDeathsSweden = data.Countries[199].TotalDeaths;
-    const NewRecoveredSweden = data.Countries[199].NewRecovered;
-    const TotalRecoveredSweden = data.Countries[199].TotalRecovered;
+    const NewConfirmedPortugal = data.Countries[166].NewConfirmed;
+    const TotalConfirmedPortugal = data.Countries[166].TotalConfirmed;
+    const NewDeathsPortugal = data.Countries[166].NewDeaths;
+    const TotalDeathsPortugal = data.Countries[166].TotalDeaths;
+    const NewRecoveredPortugal = data.Countries[166].NewRecovered;
+    const TotalRecoveredPortugal = data.Countries[166].TotalRecovered;
 
-   console.log(data.Countries);
+    const NewConfirmedSweden = data.Countries[200].NewConfirmed;
+    const TotalConfirmedSweden = data.Countries[200].TotalConfirmed;
+    const NewDeathsSweden = data.Countries[200].NewDeaths;
+    const TotalDeathsSweden = data.Countries[200].TotalDeaths;
+    const NewRecoveredSweden = data.Countries[200].NewRecovered;
+    const TotalRecoveredSweden = data.Countries[200].TotalRecovered;
 
+    
 
     document.getElementById('newCasesPoland').innerHTML = NewConfirmedPoland
     document.getElementById('allCasesPoland').innerHTML = TotalConfirmedPoland
@@ -48,6 +49,5 @@ async function getData() {
     document.getElementById('allDeathsSweden').innerHTML = TotalDeathsSweden
     document.getElementById('newCuredSweden').innerHTML = NewRecoveredSweden
     document.getElementById('allCuredSweden').innerHTML = TotalRecoveredSweden
-    
 }
 getData();
